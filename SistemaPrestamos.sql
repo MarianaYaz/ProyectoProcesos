0118b640172d5fff0bdb12f19521e604bbf4ca71
KariVI <47987287+KariVI@users.noreply.github.com>
2021-05-13 16:41:48 -0500

    Se añadio la tabla prestamo

--- SistemaPrestamos.sql
+++ SistemaPrestamos.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS Encargado(curp varchar(18) NOT NULL,
   numPersonal varchar(20) NOT NULL, turno varchar(30) NOT NULL, 
   correoElectronico varchar(100) NOT NULL, contrasenia blob NOT NULL,
   primary key (curp));
+               
+   CREATE TABLE IF NOT EXISTS Prestamo (idPrestamo int NOT NULL auto_increment, idPrestamista varchar(10) not null,
+ nombrePrestamista varchar(80) not null, fechaPrestamo varchar(15) not null, motivo varchar( 100) not null, horaPrestamo varchar(5) not null,
+ lugarPrestamo varchar(50), idDevolucion int not null auto_increment, primary key (idPrestamo),
+ foreign key (idDevolucion) REFERENCES Devolucion (idDevolucion));
+ 
