5e2f3016ad071808c9ec6f3d740bf3ce796db8a1
MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>
2021-05-13 16:39:35 -0500

    update msyql

--- SistemaPrestamos.sql
+++ SistemaPrestamos.sql
@@ -1,3 +1,9 @@
 drop database Prestamos;
 CREATE SCHEMA IF NOT EXISTS Prestamos;
 USE  Prestamos;
+
+CREATE TABLE IF NOT EXISTS Encargado(curp varchar(18) NOT NULL, 
+  nombre varchar(200) NOT NULL, 
+  numPersonal varchar(20) NOT NULL, turno varchar(30) NOT NULL, 
+  correoElectronico varchar(100) NOT NULL, contrasenia blob NOT NULL,
+  primary key (curp));
