25a4f9d2ba5a8bed9a0e13e7d0cb9341b58d6c1d
KariVI <47987287+KariVI@users.noreply.github.com>
2021-05-17 16:36:32 -0500

    Actualización del sql

--- SistemaPrestamos.sql
+++ SistemaPrestamos.sql
@@ -1,4 +1,4 @@
-drop database Prestamos;
+drop database prestamos;
 CREATE SCHEMA IF NOT EXISTS Prestamos;
 USE  Prestamos;
 
@@ -17,16 +17,18 @@ CREATE TABLE IF NOT EXISTS Jefe(curp varchar(18) NOT NULL,
 CREATE TABLE IF NOT EXISTS Credenciales(
   correoElectronico varchar(100) NOT NULL, contrasenia blob NOT NULL,
   primary key (correoElectronico));
+  
+   CREATE TABLE IF NOT EXISTS Devolucion (idDevolucion int NOT NULL auto_increment, horaDevolucion varchar(5) not null, fechaDevolucion varchar(15) not null,
+                                        comentario varchar(100) not null, presentaProblema varchar(2) not null,
+                                        PRIMARY KEY (idDevolucion));
                
    CREATE TABLE IF NOT EXISTS Prestamo (idPrestamo int NOT NULL auto_increment, idPrestamista varchar(10) not null,
  nombrePrestamista varchar(80) not null, fechaPrestamo varchar(15) not null, motivo varchar( 100) not null, horaPrestamo varchar(5) not null,
- lugarPrestamo varchar(50), idDevolucion int not null auto_increment, primary key (idPrestamo),
+ lugarPrestamo varchar(50), idDevolucion int not null, primary key (idPrestamo),
  foreign key (idDevolucion) REFERENCES Devolucion (idDevolucion));
                                                    
                                                    
- CREATE TABLE IF NOT EXISTS Devolucion (idDevolucion int NOT NULL auto_increment, horaDevolucion varchar(5) not null, fechaDevolucion varchar(15) not null,
-                                        comentario varchar(100) not null, presentaProblema varchar(2) not null,
-                                        PRIMARY KEY (idDevolucion));
+
                                                      
  
 CREATE TABLE IF NOT EXISTS ControlProyector (clave varchar(12) not null, estado varchar(15) not null, 
@@ -36,7 +38,7 @@ CREATE TABLE IF NOT EXISTS ControlProyector (clave varchar(12) not null, estado 
                                                           
 CREATE TABLE IF NOT EXISTS Laptop (clave varchar(12) not null, estado varchar(15) not null, 
                                              fechaRegistro varchar(15) not null, descripcion varchar(50) not null,
-                                             marca varchar(15) not null, modelo varchar(15) not null,
+                                             marca varchar(15) not null, modelo varchar(30) not null,
                                              PRIMARY KEY (clave));
                                                           
 CREATE TABLE IF NOT EXISTS Cable (clave varchar(12) not null, estado varchar(15) not null, 
@@ -51,13 +53,13 @@ CREATE TABLE IF NOT EXISTS Conector (clave varchar(12) not null, estado varchar(
                                                          
                                                           
 INSERT INTO Conector (clave, estado, fechaRegistro, descripcion, tipo) 
-VALUES ('FEI-CON-001', 'Disponible','2020-05-17', 'Conector HDMI color negro','HDMI);
+VALUES ('FEI-CON-001', 'Disponible','2020-05-17', 'Conector HDMI color negro','HDMI');
                                                           
 INSERT INTO Conector (clave, estado, fechaRegistro, descripcion, tipo) 
 VALUES ('FEI-CON-002', 'Disponible','2020-05-17', 'Conector VGA color azul','VGA');
                                                           
 INSERT INTO Conector (clave, estado, fechaRegistro, descripcion, tipo) 
-VALUES ('FEI-CON-003', 'Disponible','2020-05-17', 'Conector HDMI color rojo','HDMI);
+VALUES ('FEI-CON-003', 'Disponible','2020-05-17', 'Conector HDMI color rojo','HDMI');
                                                        
 INSERT INTO Laptop (clave, estado, fechaRegistro, descripcion, marca, modelo) 
 VALUES ('FEI-LAP-100', 'Disponible','2020-05-17', 'Laptop color negra para préstamos','Lenovo', 'ideapad330');
