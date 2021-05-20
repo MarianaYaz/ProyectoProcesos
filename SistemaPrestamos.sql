48a78e7976bce9d83d03c7b1bbb73bdd9438b872
KariVI <47987287+KariVI@users.noreply.github.com>
2021-05-20 16:36:41 -0500

    Actualizacion sql

--- SistemaPrestamos.sql
+++ SistemaPrestamos.sql
@@ -18,14 +18,16 @@ CREATE TABLE IF NOT EXISTS Credenciales(
   correoElectronico varchar(100) NOT NULL, contrasenia blob NOT NULL,
   primary key (correoElectronico));
   
-   CREATE TABLE IF NOT EXISTS Devolucion (idDevolucion int NOT NULL auto_increment, horaDevolucion varchar(5) not null, fechaDevolucion varchar(15) not null,
-                                        comentario varchar(100) not null, presentaProblema varchar(2) not null,
-                                        PRIMARY KEY (idDevolucion));
-               
-   CREATE TABLE IF NOT EXISTS Prestamo (idPrestamo int NOT NULL auto_increment, idPrestamista varchar(10) not null,
+CREATE TABLE IF NOT EXISTS Prestamo (idPrestamo int NOT NULL auto_increment, idPrestamista varchar(10) not null,
  nombrePrestamista varchar(80) not null, fechaPrestamo varchar(15) not null, motivo varchar( 100) not null, horaPrestamo varchar(5) not null,
- lugarPrestamo varchar(50), idDevolucion int not null, primary key (idPrestamo),
- foreign key (idDevolucion) REFERENCES Devolucion (idDevolucion));
+ lugarPrestamo varchar(50), idDevolucion int not null, primary key (idPrestamo));
+  
+   CREATE TABLE IF NOT EXISTS Devolucion (idDevolucion int NOT NULL auto_increment, horaDevolucion varchar(5) not null, fechaDevolucion varchar(15) not null,
+                                        comentario varchar(100) not null, presentaProblema varchar(2) not null, idPrestamo int NOT NULL,
+                                        PRIMARY KEY (idDevolucion), foreign key (idPrestamo) REFERENCES Prestamo(idPrestamo));
+               
+
+ 
                                                    
                                                    
 
@@ -88,3 +90,9 @@ VALUES ('FEI-CAB-155', 'Disponible','2020-05-17', 'Cable de ethernet color azul'
                                                           
 INSERT INTO Cable (clave, estado, fechaRegistro, descripcion, tipo) 
 VALUES ('FEI-CAB-156', 'Disponible','2020-05-17', 'Cable USB de tipo C color negro','USB-C'); 
+
+
+
+create user 'empleado'@'localhost' IDENTIFIED BY 'password1';
+
+GRANT SELECT,INSERT,DELETE,UPDATE ON prestamos.* TO 'empleado'@'localhost';
