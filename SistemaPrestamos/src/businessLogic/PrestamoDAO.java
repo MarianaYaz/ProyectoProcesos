78e22853f05e16add4bfa80c080d95fafe856200
KariVI <47987287+KariVI@users.noreply.github.com>
2021-05-20 16:39:54 -0500

    Se agregaron datos

--- SistemaPrestamos/src/businessLogic/PrestamoDAO.java
+++ SistemaPrestamos/src/businessLogic/PrestamoDAO.java
@@ -5,10 +5,49 @@
  */
 package businessLogic;
 
+import dataAccess.ConnectorDB;
+import domain.Prestamo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author kari
  */
 public class PrestamoDAO {
     
+    
+    
+    /* CREATE TABLE IF NOT EXISTS Prestamo (idPrestamo int NOT NULL auto_increment, idPrestamista varchar(10) not null,
+ nombrePrestamista varchar(80) not null, fechaPrestamo varchar(15) not null, motivo varchar( 100) not null, horaPrestamo varchar(5) not null,
+ lugarPrestamo varchar(50), idDevolucion int not null, primary key (idPrestamo),
+ foreign key (idDevolucion) REFERENCES Devolucion (idDevolucion));*/
+    public boolean guardadoExitoso(Prestamo prestamo)  {
+        boolean value=false;
+            try {
+                ConnectorDB connectorDataBase=new ConnectorDB();
+                Connection connectionDataBase = connectorDataBase.getConnection();
+                String insertGroupAcademic = "INSERT INTO CuerpoAcademico(clave,nombre, objetivo, mision , vision , gradoConsolidacion) VALUES (?,?,?,?,?,?)";
+            
+                PreparedStatement preparedStatement = connectionDataBase.prepareStatement(insertGroupAcademic);
+                
+               /* preparedStatement.setString(1,);
+                preparedStatement.setString(2,);
+                preparedStatement.setString(3);
+                preparedStatement.setString(4);
+                preparedStatement.setString(5);
+                preparedStatement.setString(6);*/
+                
+                
+                preparedStatement.executeUpdate();
+                connectorDataBase.disconnect();
+                value=true;
+            } catch (SQLException sqlException) {
+               // throw new BusinessException("DataBase connection failed ", sqlException);
+            } catch (ClassNotFoundException ex) {
+              // Log.logException(ex);
+            }
+        return value;
+    }
 }
