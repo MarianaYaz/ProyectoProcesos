d30722216c23a350325d98eea25445accc4ad3a5
KariVI <47987287+KariVI@users.noreply.github.com>
2021-05-27 15:13:30 -0500

    Revert "Add files"
    This reverts commit 2fa43262ce91fcbed46984d091e2b55a4d7a1180.

--- SistemaPrestamos/src/domain/Conector.java
+++ SistemaPrestamos/src/domain/Conector.java
@@ -7,6 +7,11 @@ package domain;
  */
 public class Conector extends Dispositivo{
     private String tipo;
+    
+    public Conector(String tipo, String clave, String descripcion, String fechaRegistro, String estado){
+        super(clave, descripcion, fechaRegistro, estado);
+        this.tipo = tipo;
+    }
 
     public String getTipo() {
         return tipo;
